68efcf1065d06b407a6e36c0dd4873b655f85796
shamrat17 <shamratakbar17@gmail.com>
2019-05-02 15:19:46 +0600

    updated tests

--- test/api.spec.js
+++ test/api.spec.js
@@ -8,11 +8,19 @@ describe('Test the available api', () => {
                 seedFile: `${__dirname}/../data/dummy.sql`
             }
         });
+        expect(result.isDone).toEqual(true)
+    });
+    test('Fresh DB and seed sql', () => {
+        const result = runMySql({
+            type: 'fresh',
+        });
+        expect(result.isDone).toEqual(false)
     });
     test('Truncate DB', () => {
         const result = runMySql({
             type: 'truncate'
         });
+        expect(result.isDone).toEqual(true)
     });
     test('Execute sql', () => {
         const result = runMySql({
@@ -21,5 +29,6 @@ describe('Test the available api', () => {
                 seedFile: `${__dirname}/../data/dummy.sql`
             }
         });
+        expect(result.isDone).toEqual(true)
     });
 })
